2e4cb36b3c8deaf2917100b87e3f69a3d84482b2
Ramya kocharlakota <Ramyakocharlakota@gmail.com>
2021-12-25 15:04:13 +0530

    ADDED

---
+++ Fibonacciseries.java/src/com/fibonacci/fibonacciseries.java
@@ -0,0 +1,27 @@
+package com.fibonacci;
+
+import java.util.Scanner;
+
+public class fibonacciseries {
+		static void Fibonacci(int count) {
+			int num1 = 0, num2 = 1;
+			int counter = 0;
+			while (counter < count) {
+				System.out.print(num1 + " ");
+				int num3 = num2 + num1;
+				num1 = num2;
+				num2 = num3;
+				counter = counter + 1;
+			}
+		}
+		public static void main (String[] args) {
+			Scanner sc = new Scanner(System.in);
+			System.out.println( "How many fibonnaci elements to print ?" );
+			int count = sc.nextInt(); 
+			System.out.println( "fibonnaci elements are :" );
+			Fibonacci(count);
+		// TODO Auto-generated method stub
+
+	}
+
+}
