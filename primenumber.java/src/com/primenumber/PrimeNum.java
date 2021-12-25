488887b239c056dc1ff07cfa7337de583d9a4d41
Ramya kocharlakota <Ramyakocharlakota@gmail.com>
2021-12-25 15:18:19 +0530

    ADDED

---
+++ primenumber.java/src/com/primenumber/PrimeNum.java
@@ -0,0 +1,24 @@
+package com.primenumber;
+
+import java.util.Scanner;
+
+public class PrimeNum {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);         
+		System.out.print("Enter the number: ");  
+		int primeNum = sc.nextInt();  
+		
+		for(int i = 2; i <= primeNum/2; i++) {
+			if(primeNum%i == 0) {
+				System.out.print(primeNum+" is not a prime number.");  
+			}else {
+				System.out.print(primeNum+" is a prime number. "); 
+			}
+			break;
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
