dae2e6e4732aacc8993b4422fa483d65aa618ea9
Ramya kocharlakota <Ramyakocharlakota@gmail.com>
2021-12-25 15:24:45 +0530

    ADDED

---
+++ ReverseNum.java/src/com/reversenum/ReverseNum.java
@@ -0,0 +1,22 @@
+package com.reversenum;
+
+import java.util.Scanner;
+
+public class ReverseNum {
+
+	public static void main(String[] args) {
+		System.out.println("Enter the number to be reversed :");  
+		Scanner sc = new Scanner(System.in);
+			int number = sc.nextInt();
+			int reverse = 0;  
+			while(number != 0)   
+			{  
+			int remainder = number % 10;  
+			reverse = reverse * 10 + remainder;  
+			number = number/10;  
+			}  
+			System.out.println("The reverse of the given number is : " + reverse);  
+	}
+		// TODO Auto-generated method stub
+
+	}
